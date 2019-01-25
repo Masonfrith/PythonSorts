221295c97ac17278b3b14072456e884dee8ee593
Masonfrith <gedsaro@gmail.com>
2019-01-25 13:08:33 -0600

    create signatture for new function 'insert_Sort(x)'...
    create interface doc for method.

--- PythonSorts/PythonSorts.py
+++ PythonSorts/PythonSorts.py
@@ -1,1 +1,20 @@
 
+
+def insert_Sort(dataToSort):
+    """
+    Purpose:
+        Given a numeric, or text, data sequence (dataToSort), return a new sorted list using
+        insertion sort to do so.
+    Preconditions:
+        dataToSort: a data sequence whose elements must be text, or base 10
+        numeric data, or empty.
+    Post-condition:
+        A new list is made which will contain the same elements passed by
+        dataToSort, but will be returned sorted.
+        !!! The origional sequence given may be altered or even deleted by this
+        method !!!
+    Return:
+        sortedListData: a new list sorted in acending order made from the elements
+        in dataToSort, if given an empty set, returns a new empty list.
+        all numeric data is treated as coming before text data, eg 999 comes before '0a'
+    """
