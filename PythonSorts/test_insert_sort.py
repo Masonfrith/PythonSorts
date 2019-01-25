a34fbf296f108509392edc48ab214bc25dea8e05
Masonfrith <gedsaro@gmail.com>
2019-01-25 14:52:12 -0600

    Rename current Tests for readability...
    Add 4 new tests for passing different kinds of none sequence data to the sort, some which should be vaild and returned as a list with a single item, some which should be rejected as unsupported data.
    Tests not yet implemented.

--- PythonSorts/test_insert_sort.py
+++ PythonSorts/test_insert_sort.py
@@ -2,17 +2,43 @@ import unittest
 import PythonSorts as ps
 
 class insert_sort_Test(unittest.TestCase):
-    def test_InsertSortEmptyList(self):
+    def test_EmptyList_InsertSort(self):
         aList = list()
         sList = ps.insert_Sort(aList)
         self.assertEqual(sList, [], 
                          "Insert_Sort did not return an empty list!")
 
-    def test_InsertSortNoneData(self):
+    def test_NoneData_InsertSort(self):
         x = None
         sList = ps.insert_Sort(x)
         self.assertIs(sList, None,
                       'insert_sort should return value None without error when passed data with value None')
+
+    def test_LoneNumbers_InsertSort(self):
+        aInt = -4
+        aFloat = -0.1159
+        aZero = 0
+        aPositiveInt = 100089
+        self.fail("Not Implemented Yet")
+
+    def test_LoneStrings_InsertSort(self):
+        aEmpty = ""
+        aSpace = " "
+        aChar = "c"
+        aWord = "Word"
+        aLongString = "This is a really long string with spaces and a \n newline!"
+        self.fail("Not Implemented Yet")
+
+    def test_LoneStringNumerics_InsertSort(self):
+        aIntString = "-47"
+        aFloatString = "-0.15647"
+        aStringWithNumbers = "5Five"
+        self.fail("Not Implemented Yet")
+
+    def test_InvalidLoneData_InsertSort(self):
+        boolTrue = True
+        boolFalse = False
+        self.fail("Not Implemented Yet")
 
 if __name__ == '__main__':
     unittest.main()
